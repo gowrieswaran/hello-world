d06b2b90c15d6da889f4559af757f3e33e92086d
gowrieswaran <gowrieswar16@gmail.com>
2018-11-21 22:46:18 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+//import the http package
+const http = require("http");
+
+const hostname = "127.0.0.1";
+const port = process.env.port || 3000;
+
+http
+  .createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Hello World");
+  })
+  .listen(port, hostname, () => {
+    console.log(`Server is running at http://${hostname}:${port}/`);
+  });
