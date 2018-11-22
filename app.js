e45edf5006489b3bd40a7d5a77471ff9e4030ecf
gowrieswaran <gowrieswar16@gmail.com>
2018-11-22 17:02:43 +0530

    port commented out

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 //import the http package
 const http = require("http");
 
-const hostname = "127.0.0.1";
-const port = process.env.port || 3000;
+//const hostname = "127.0.0.1";
+const port = process.env.port;
 
 http
   .createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/plain" });
     res.end("Hello World");
   })
-  .listen(port, hostname, () => {
-    console.log(`Server is running at http://${hostname}:${port}/`);
+  .listen(port, () => {
+    console.log(`Server is running at port ${port}/`);
   });
